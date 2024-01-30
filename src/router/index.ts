0ce67d6a55ee28c54bd85009c8dca813e792e054
Alex Venus <mr.taff@ya.ru>
2024-01-30 23:34:10 +0300

    Payments

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Finances from '../views/Finances.vue'
 import Cards from '../views/Cards.vue'
+import Payments from '../views/Payments.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/cards',
       name: 'cards',
       component: Cards
+    },
+    {
+      path: '/payments',
+      name: 'payments',
+      component: Payments
     }
     // {
     //   path: '/about',
---
+++ src/views/Payments.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Header from '../components/common/Header/Header.vue'
+import PaymentList from '../components/common/PaymentList/PaymentList.vue'
+</script>
+
+<template>
+    <Header title="Платежи" />
+    <section class="tw-flex tw-flex-col">
+        <PaymentList />
+    </section>
+</template>
