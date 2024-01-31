f5eec321885941f87c200efe36ef73d56a3f10a4
Alex Venus <mr.taff@ya.ru>
2024-01-31 17:46:24 +0300

    Clickable menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
-import { RouterView } from 'vue-router'
+import { ref, reactive } from 'vue'
+import { RouterView, useRouter } from 'vue-router'
+import { getNavItemStroke } from '@/utils'
 import ArrowLeft from './components/icons/ArrowLeft.vue'
 import ArrowRight from './components/icons/ArrowRight.vue'
 import Squares from './components/icons/Squares.vue'
@@ -9,12 +10,52 @@ import ArrowsLeftRight from './components/icons/ArrowsLeftRight.vue'
 import Warning from './components/icons/Warning.vue'
 import Mobile from './components/icons/Mobile.vue'
 
+const $router = useRouter()
+
 const navOptions = reactive({
   drawer: true,
   rail: true,
   minWidth: 74,
   maxWidth: 297
 })
+
+const navItems = ref([
+  {
+    id: 0,
+    title: 'Финансы',
+    path: '/finances',
+    name: 'finances',
+    icon: Squares
+  },
+  {
+    id: 1,
+    title: 'Банковские карты',
+    path: '/cards',
+    name: 'cards',
+    icon: CreditCard
+  },
+  {
+    id: 2,
+    title: 'Платежи',
+    path: '/payments',
+    name: 'payments',
+    icon: ArrowsLeftRight
+  },
+  {
+    id: 3,
+    title: 'Диспуты',
+    path: '/disputs',
+    name: 'disputs',
+    icon: Warning
+  },
+  {
+    id: 4,
+    title: 'Устройства',
+    path: '/devices',
+    name: 'devices',
+    icon: Mobile
+  }
+])
 </script>
 
 <template>
@@ -39,15 +80,26 @@ const navOptions = reactive({
             </div>
 
             <v-list density="compact" nav>
-              <v-list-item value="home">
+              <template v-for="navItem in navItems" :key="navItem.id">
+                <v-list-item :value="navItem.name" active-color="#F8FCFE" @click="$router.push(navItem.path)">
+                  <template v-slot:prepend>
+                    <component :is="navItem.icon" v-bind="{ stroke: getNavItemStroke(navItem.name, $route.name as string, { active: '#04B6F5', default: '#2B3A4C' }) }"></component>
+                    <!-- <Squares :stroke="$route.name === navItem.name ? '#04B6F5' : '#2B3A4C'" /> -->
+                  </template>
+                  <template v-slot:title>
+                    <span class="tw-ml-2 tw-text-[15px]" v-show="!navOptions.rail">{{ navItem.title }}</span>
+                  </template>
+                </v-list-item>
+              </template>
+              <!-- <v-list-item value="finances" active-color="#F8FCFE" @click="$router.push('/finances')">
                 <template v-slot:prepend>
-                  <Squares />
+                  <Squares :stroke="$route.name === 'finances' ? '#04B6F5' : '#2B3A4C'" />
                 </template>
                 <template v-slot:title>
                   <span class="tw-ml-2 tw-text-[15px]" v-show="!navOptions.rail">Финансы</span>
                 </template>
               </v-list-item>
-              <v-list-item value="account">
+              <v-list-item value="cards" active-color="#F8FCFE" @click="$router.push('/cards')">
                 <template v-slot:prepend>
                   <CreditCard />
                 </template>
@@ -55,7 +107,7 @@ const navOptions = reactive({
                   <span class="tw-ml-2 tw-text-[15px]" v-show="!navOptions.rail">Банковские карты</span>
                 </template>
               </v-list-item>
-              <v-list-item value="account">
+              <v-list-item value="payments">
                 <template v-slot:prepend>
                   <ArrowsLeftRight />
                 </template>
@@ -63,7 +115,7 @@ const navOptions = reactive({
                   <span class="tw-ml-2 tw-text-[15px]" v-show="!navOptions.rail">Платежи</span>
                 </template>
               </v-list-item>
-              <v-list-item value="account">
+              <v-list-item value="disputs">
                 <template v-slot:prepend>
                   <Warning />
                 </template>
@@ -71,14 +123,14 @@ const navOptions = reactive({
                   <span class="tw-ml-2 tw-text-[15px]" v-show="!navOptions.rail">Диспуты</span>
                 </template>
               </v-list-item>
-              <v-list-item value="account">
+              <v-list-item value="devices">
                 <template v-slot:prepend>
                   <Mobile />
                 </template>
                 <template v-slot:title>
                   <span class="tw-ml-2 tw-text-[15px]" v-show="!navOptions.rail">Устройства</span>
                 </template>
-              </v-list-item>
+              </v-list-item> -->
             </v-list>
           </div>
           <div class="tw-flex tw-items-center tw-gap-x-4 tw-w-full tw-cursor-pointer" v-show="!navOptions.rail" @click="navOptions.rail = !navOptions.rail">
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
 export const formatter = new Intl.NumberFormat("ru", {
     minimumFractionDigits: 2
 });
+
+export const getNavItemStroke = (navItemRouteName: string, currentRouteName: string, colors: { default: string, active: string }) => {
+    return navItemRouteName === currentRouteName ? colors.active : colors.default
+}
