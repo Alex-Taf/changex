9996702b8f10c84a7d7014068898835e5abc60d7
Alex Venus <mr.taff@ya.ru>
2024-01-31 17:50:49 +0300

    Menu nav-item label fix

--- src/App.vue
+++ src/App.vue
@@ -84,53 +84,12 @@ const navItems = ref([
                 <v-list-item :value="navItem.name" active-color="#F8FCFE" @click="$router.push(navItem.path)">
                   <template v-slot:prepend>
                     <component :is="navItem.icon" v-bind="{ stroke: getNavItemStroke(navItem.name, $route.name as string, { active: '#04B6F5', default: '#2B3A4C' }) }"></component>
-                    <!-- <Squares :stroke="$route.name === navItem.name ? '#04B6F5' : '#2B3A4C'" /> -->
                   </template>
                   <template v-slot:title>
-                    <span class="tw-ml-2 tw-text-[15px]" v-show="!navOptions.rail">{{ navItem.title }}</span>
+                    <span class="tw-ml-2 tw-text-[15px] tw-text-[#2B3A4C]" v-show="!navOptions.rail">{{ navItem.title }}</span>
                   </template>
                 </v-list-item>
               </template>
-              <!-- <v-list-item value="finances" active-color="#F8FCFE" @click="$router.push('/finances')">
-                <template v-slot:prepend>
-                  <Squares :stroke="$route.name === 'finances' ? '#04B6F5' : '#2B3A4C'" />
-                </template>
-                <template v-slot:title>
-                  <span class="tw-ml-2 tw-text-[15px]" v-show="!navOptions.rail">Финансы</span>
-                </template>
-              </v-list-item>
-              <v-list-item value="cards" active-color="#F8FCFE" @click="$router.push('/cards')">
-                <template v-slot:prepend>
-                  <CreditCard />
-                </template>
-                <template v-slot:title>
-                  <span class="tw-ml-2 tw-text-[15px]" v-show="!navOptions.rail">Банковские карты</span>
-                </template>
-              </v-list-item>
-              <v-list-item value="payments">
-                <template v-slot:prepend>
-                  <ArrowsLeftRight />
-                </template>
-                <template v-slot:title>
-                  <span class="tw-ml-2 tw-text-[15px]" v-show="!navOptions.rail">Платежи</span>
-                </template>
-              </v-list-item>
-              <v-list-item value="disputs">
-                <template v-slot:prepend>
-                  <Warning />
-                </template>
-                <template v-slot:title>
-                  <span class="tw-ml-2 tw-text-[15px]" v-show="!navOptions.rail">Диспуты</span>
-                </template>
-              </v-list-item>
-              <v-list-item value="devices">
-                <template v-slot:prepend>
-                  <Mobile />
-                </template>
-                <template v-slot:title>
-                  <span class="tw-ml-2 tw-text-[15px]" v-show="!navOptions.rail">Устройства</span>
-                </template>
-              </v-list-item> -->
             </v-list>
           </div>
           <div class="tw-flex tw-items-center tw-gap-x-4 tw-w-full tw-cursor-pointer" v-show="!navOptions.rail" @click="navOptions.rail = !navOptions.rail">
