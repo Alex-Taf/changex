f31d4b4565c50d8ca7f6d306ae1b85a2c80a3bb2
Alex Venus <mr.taff@ya.ru>
2024-02-02 19:23:30 +0300

    Adaptive pt1

--- src/components/common/FinancesBalance/FinancesBalance.vue
+++ src/components/common/FinancesBalance/FinancesBalance.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import FinancesBalanceIcon from '../../icons/FinancesBalanceIcon.vue'
 import FinancesDialog from '../FinancesDialog/FinancesDialog.vue'
-import { ref } from 'vue';
+import RenderOn from '@/components/utils/RenderOn.vue'
 
 const dialogOpen = ref(false)
 
@@ -14,7 +15,7 @@ function closeDialog(close: boolean) {
     <v-card class="tw-w-full tw-h-[132px] !tw-rounded-2xl !tw-p-[24px] tw-mb-6">
         <section class="tw-flex tw-justify-between tw-items-center">
             <section class="tw-flex tw-items-center tw-gap-x-4">
-                <FinancesBalanceIcon />
+                <RenderOn :px="1280"><FinancesBalanceIcon /></RenderOn>
                 <div class="tw-flex tw-flex-col tw-leading-7">
                     <span class="tw-text-[13px] tw-text-[#AEB7C1]">Баланс</span>
                     <span>
@@ -23,14 +24,20 @@ function closeDialog(close: boolean) {
                     <span class="tw-text-[13px] tw-text-[#677483]">~65 238 894 RUB</span>
                 </div>
             </section>
-            <v-btn class="!tw-rounded-xl !tw-w-[172px] !tw-h-[52px] hover:!tw-shadow-[0px_10px_18px_2px_rgba(4,182,245,0.2)]" color="#04B6F5" variant="elevated" @click="dialogOpen = true">
+            <v-btn class="!tw-rounded-xl !tw-min-w-[52px] min-lg:!tw-w-[172px] min-lg:!tw-h-[52px]
+                            sm:!tw-w-[52px] md:!tw-w-[52px] sm:!tw-h-[52px] md:!tw-h-[52px]
+                            hover:!tw-shadow-[0px_10px_18px_2px_rgba(4,182,245,0.2)]"
+                    color="#04B6F5"
+                    variant="elevated"
+                    @click="dialogOpen = true"
+            >
                 <template v-slot:prepend>
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M3.125 10H16.875" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
                         <path d="M10 3.125V16.875" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                 </template>
-                <span class="tw-text-white tw-text-[16px] tw-normal-case tw-tracking-normal tw-leading-4">Пополнить</span>
+                <span class="sm:tw-hidden md:tw-hidden min-lg:tw-inline tw-text-white tw-text-[16px] tw-normal-case tw-tracking-normal tw-leading-4">Пополнить</span>
             </v-btn>
         </section>
     </v-card>
